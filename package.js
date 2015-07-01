8957302374fc3aab8db4d7e59e6e9be5c8f293da
Kelly <copleykj@gmail.com>
2015-06-30 23:18:49 -0400

    bump version

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
     name: 'socialize:user-presence',
     summary: 'Scalable user presence',
-    version: '0.2.0',
+    version: '0.2.1',
     git: 'https://github.com/copleykj/socialize-user-presence.git'
 });
 
