beef94928e6c3e0dafc26ab2c7a6f6be39be7cf8
copleykj <copleykj@gmail.com>
2016-02-21 00:18:13 -0500

    patch version bump

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
     name: 'socialize:user-presence',
     summary: 'Scalable user presence',
-    version: '0.3.2',
+    version: '0.3.3',
     git: 'https://github.com/copleykj/socialize-user-presence.git'
 });
 
