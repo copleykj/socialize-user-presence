095626cf4f22df0221b806f401fe9e8447bf43a1
copleykj <copleykj@gmail.com>
2016-06-07 01:23:27 -0400

    pass connection to onSessionConnected and onSessionDisconnected

--- server/publications.js
+++ server/publications.js
@@ -2,11 +2,11 @@ Meteor.publish(null, function(){
     var self = this;
     if(self.userId && self.connection && self.connection.id){
         userConnected(self.connection.id, self.userId, ServerPresence.serverId(), self.connection);
-        sessionConnected(self.connection.id);
+        sessionConnected(self.connection);
 
         self.onStop(function(){
             userDisconnected(self.connection.id, self.userId, self.connection);
-            sessionDisconnected(self.connection.id);
+            sessionDisconnected(self.connection);
         });
     }
     self.ready();
