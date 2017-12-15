1129b88e233a69ab44cfa4291e93d6bef783e892
Kelly Copley <copleykj@gmail.com>
2017-12-14 23:00:06 -0500

    use theck for method argument

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 /* eslint-disable import/no-unresolved */
 import { Meteor } from 'meteor/meteor';
+import { check, Match } from 'meteor/check';
 
 /* eslint-enable import/no-unresolved */
 
@@ -11,6 +12,7 @@ import '../common/user-extensions.js';
 
 Meteor.methods({
     updateSessionStatus(status) {
+        check(status, Match.Integer);
         if (this.userId && (status === 1 || status === 2)) {
             UserSessions.update(this.connection.id, { $set: { status } });
             determineStatus(this.userId);
