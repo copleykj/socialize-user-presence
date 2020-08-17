b594db736901b121bfc69dc15efb83b83ffc79c4
Kelly Copley <copleykj@gmail.com>
2020-08-17 09:16:03 -0400

    fix publishing node_modules

--- package.js
+++ package.js
@@ -2,14 +2,14 @@
 Package.describe({
     name: 'socialize:user-presence',
     summary: 'Scalable user presence',
-    version: '1.0.2',
+    version: '1.0.3',
     git: 'https://github.com/copleykj/socialize-user-presence.git',
 });
 
 Package.onUse(function _(api) {
     api.versionsFrom('1.10.2');
     api.use(['mongo']);
-    api.use(['socialize:server-presence@1.0.2', 'socialize:user-model@1.0.3']);
+    api.use(['socialize:server-presence@1.0.2', 'socialize:user-model@1.0.4']);
 
     api.imply('socialize:user-model');
 
