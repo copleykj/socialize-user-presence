3e0def639e4b7142c51267613961bc787739a02c
Kelly Copley <copleykj@gmail.com>
2021-06-28 00:02:06 -0400

    update deps and versionsFrom for compatibility

--- package.js
+++ package.js
@@ -2,14 +2,14 @@
 Package.describe({
     name: 'socialize:user-presence',
     summary: 'Scalable user presence',
-    version: '1.0.3',
+    version: '1.0.4',
     git: 'https://github.com/copleykj/socialize-user-presence.git',
 });
 
 Package.onUse(function _(api) {
-    api.versionsFrom('1.10.2');
+    api.versionsFrom(['1.10.2', '2.3']);
     api.use(['mongo']);
-    api.use(['socialize:server-presence@1.0.2', 'socialize:user-model@1.0.4']);
+    api.use(['socialize:server-presence@1.0.4', 'socialize:user-model@1.0.5']);
 
     api.imply('socialize:user-model');
 
